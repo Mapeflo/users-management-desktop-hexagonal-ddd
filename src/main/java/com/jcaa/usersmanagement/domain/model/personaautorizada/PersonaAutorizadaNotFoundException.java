a6e711bca1332e0a4014ac891aced42beed56158
mapeflo <mpenarandaf@unicartagena.edu.co>
2026-05-29 12:17:00 -0500

    feat: agregar capa de dominio completa para persona_autorizada (entidad, value objects, excepción y repositorio)

--- src/main/java/com/jcaa/usersmanagement/domain/model/personaautorizada/PersonaAutorizadaNotFoundException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/personaautorizada/PersonaAutorizadaNotFoundException.java
@@ -1,4 +1,12 @@
 package com.jcaa.usersmanagement.domain.model.personaautorizada;
 
-public class PersonaAutorizadaNotFoundException {
+public class PersonaAutorizadaNotFoundException extends RuntimeException {
+
+    public PersonaAutorizadaNotFoundException(String dni) {
+        super("No se encontró persona autorizada con DNI: " + dni);
+    }
+
+    public PersonaAutorizadaNotFoundException(Long id) {
+        super("No se encontró persona autorizada con ID: " + id);
+    }
 }
--- src/main/java/com/jcaa/usersmanagement/domain/model/personaautorizada/PersonaAutorizadaRepository.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/personaautorizada/PersonaAutorizadaRepository.java
@@ -1,4 +1,21 @@
 package com.jcaa.usersmanagement.domain.model.personaautorizada;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PersonaAutorizadaRepository {
+
+    PersonaAutorizada save(PersonaAutorizada personaAutorizada);
+
+    Optional<PersonaAutorizada> findById(Long id);
+
+    Optional<PersonaAutorizada> findByDni(String dni);
+
+    List<PersonaAutorizada> findAll();
+
+    List<PersonaAutorizada> findByNinoId(Long ninoId);   // Caso de uso adicional
+
+    void delete(Long id);
+
+    boolean existsByDni(String dni);
 }
